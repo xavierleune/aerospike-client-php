115480dc094f9f55fdeb4df0285f36013ae4df40
root <root@localhost.localdomain>
2014-07-03 18:42:57 +0530

    Loginfo of PUT and GET operation will write to file only once at the end of script.

--- src/aerospike/performance/benchmark_get.php
+++ src/aerospike/performance/benchmark_get.php
@@ -1,33 +1,33 @@
 <?php
 $keys = $argv[1];
-$handle = fopen ("benchmark_get",'a+');
 if ($keys < 10000) {
 	echo "Enter first argument greater then or equal to 10000.\n";
 }else {
 	$config = array("hosts"=>array(array("name"=>"10.71.71.49", "port"=>3000)));
-	
+	$line = "";	
 	$as = new Aerospike($config);
-	$r=new Record();
-	//$r = array();
+	$r = array();
 	$avg=0;
+	$count = 0;
 	for($i=10000;$i<=$keys;$i++)
 	{
-		$key = array("ns"=>"test", "set"=>"demo", "key"=>"'$i'");
-		
-		fwrite($handle , "Memory Usage Get:= ".memory_get_usage().", ");
+		$key = array("ns"=>"test", "set"=>"demo", "key"=>"$i");
+		$line = $line."Memory Usage Get:= ".memory_get_usage().", ";
 		$start = microtime(true);
-		fwrite($handle , "Start GET MicroS:= ".$start.", ");
+		$line = $line."Start GET MicroS:= ".$start.", ";
 		$as->get($key, &$r);
-		$rs = $r->getBins();
 		$end = microtime(true);
-		fwrite($handle , "End  GET MicroS:= ".$end.", ");
+		$line = $line."End  GET MicroS:= ".$end.", ";
 		$avg = $avg+($end-$start);
-		fwrite($handle , "Diff in MicroS:= ".($end-$start).", ");
-		fwrite($handle , "Memory Usage end Get:= ".memory_get_usage()."\n");
+		$line = $line."Diff in MicroS:= ".($end-$start).", ";
+		$line = $line."Memory Usage end Get:= ".memory_get_usage()."\n";
+		$count++;
 	}
-	$average_get_time = $avg/99000;
-	fwrite($handle , "Total GET Time:= ".$avg."\n");;
-	fwrite($handle , "Total AVG GET Time:= ".$average_get_time."\n");;
+	$average_get_time = $avg/$count;
+	$line = $line."\nTotal GET Time:= ".$avg."\n";
+	$line = $line."Total AVG GET Time:= ".$average_get_time."\n";
 }
+$handle = fopen("GetPerformance",'a+');
+fwrite($handle,$line);
 fclose($handle);
 ?>
--- src/aerospike/performance/benchmark_put.php
+++ src/aerospike/performance/benchmark_put.php
@@ -8,15 +8,33 @@ if ($keys < 10000) {
 }else {
 	$config = array("hosts"=>array(array("name"=>"10.71.71.49", "port"=>3000)));
 	$as = new Aerospike($config);
+	$avg=0;
+        $count = 0;
+	$line="";
 	for($i=10000;$i<=$keys;$i++)
 	{
-		$key = array("ns"=>"test", "set"=>"demo", "key"=>"'$i'");
-		for($j=1;$j<=count($numberofbins);$j++)
+		$key = array("ns"=>"test", "set"=>"demo", "key"=>"$i");
+		for($j=1;$j<=$numberofbins;$j++)
 		{
 			$value = $i+$j;
-			$bins_key_value_array["bin".$j] = "'$value'";
+			$bins_key_value_array["bin".$j] = "$value";
 		}
+                $line = $line."Memory Usage Put:= ".memory_get_usage().", ";
+                $start = microtime(true);
+                $line = $line."Start PUT MicroS:= ".$start.", ";
 		$rv = $as->put($key, $bins_key_value_array);
-	}
-}	
+		$end = microtime(true);
+                $line = $line."End  PUT MicroS:= ".$end.", ";
+                $avg = $avg+($end-$start);
+                $line = $line."Diff in MicroS:= ".($end-$start).", ";
+                $line = $line."Memory Usage end Put:= ".memory_get_usage()."\n";
+                $count++;
+        }
+        $average_put_time = $avg/$count;
+        $line = $line."\nTotal PUT Time:= ".$avg."\n";
+        $line = $line."Total AVG PUT Time:= ".$average_put_time."\n";
+}
+$handle = fopen("PutPerformance",'a+');
+fwrite($handle,$line);
+fclose($handle);	
 ?>
