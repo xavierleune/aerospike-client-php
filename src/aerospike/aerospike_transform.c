f8adf724ab2c16e191d87e06b99f2f4e713577e4
PavanRao-GSLab <pavan.rao@gslab.com>
2014-07-10 00:27:28 -0700

    uncompiled code for put

--- src/aerospike/aerospike_transform.c
+++ src/aerospike/aerospike_transform.c
@@ -280,17 +280,16 @@ exit:
     return status;
 }
 
-#if 0
 typedef struct data_list_map {
-    u_int32_t   current_list_idx_u32;
-    as_list     alloc_list[MAX_AS_LIST_SIZE];
-    u_int32_t   current_map_idx_u32;
-    as_map  alloc_map[MAX_AS_MAP_SIZE];
+    u_int32_t        current_list_idx_u32;
+    as_arraylist     alloc_list[MAX_AS_LIST_SIZE];
+    u_int32_t        current_map_idx_u32;
+    as_hashmap       alloc_map[MAX_AS_MAP_SIZE];
 } as_data_list_map_struct;
 
 extern as_status
-aerospike_switch_key_type(as_key* as_key_p,
-                          int8_t* namespace_p, int8_t* set_p, zval** record_pp)
+aerospike_add_key_params(as_key* as_key_p,
+                         int8_t* namespace_p, int8_t* set_p, zval** record_pp)
 {
     as_status      status = AEROSPIKE_OK;
 
@@ -304,7 +303,7 @@ aerospike_switch_key_type(as_key* as_key_p,
             as_key_init_int64(as_key_p, namespace_p, set_p, (int64_t) Z_LVAL_PP(record_pp));
             break;
         case IS_STRING:
-            as_key_init_str(&key, namespace, set, (int8_t *) Z_STRVAL_PP(record_key));
+            as_key_init_str(&key, namespace, set, (int8_t *) Z_STRVAL_PP(record_pp));
             break;
         default:
             status = AEROSPIKE_ERR;
@@ -315,34 +314,170 @@ exit:
     return status;
 }
 
-static as_status
-aerospike_transform_fill_data(HashTable* ht_p, as_record* record_p, zval** record_pp)
-{
-    as_status         status = AEROSPIKE_OK;
+#define GET_PRE_ALLOC_LIST(list_p, pre_stackalloc_data_p)                                           \
+     if (MAX_AS_LIST_SIZE > pre_stackalloc_data_p->current_list_idx_u32) {                          \
+         list_p = pre_stackalloc_data_p->alloc_list[pre_stackalloc_data_p->current_list_idx_u32];   \
+     }
+#define GET_PRE_ALLOC_MAP(map_p, pre_stackalloc_data_p)                                             \
+     if (MAX_AS_LIST_SIZE > pre_stackalloc_data_p->current_map_idx_u32) {                           \
+         map_p = pre_stackalloc_data_p->alloc_map[pre_stackalloc_data_p->current_map_idx_u32];      \
+     }
+#define AS_APPEND_INT(as_val, data_int) \
+     if (AS_MAP == as_val_type(as_val)) { \
+     } else if (AS_LIST == as_val_type(as_val)) { \
+         as_arraylist_append_int64(list, Z_LVAL_PP(data)); \
+     } else {\
+         status = AEROSPIKE_ERR; \
+         goto exit;     \
+     }
+#define AS_APPEND_STR(as_val, data_int) \
+     if (AS_MAP == as_val_type(as_val)) { \
+     } else if (AS_LIST == as_val_type(as_val)) { \
+         as_arraylist_append_str(list, Z_STRVAL_PP(data)); \
+     } else {\
+         status = AEROSPIKE_ERR; \
+         goto exit;     \
+     }
 
-    if ((!ht_p) || (!record_p) || (!record_pp)) {
+#define AS_LIST_I
+
+#define AS_INIT_DATA_STORE(as_val) \
+     if (AS_MAP == as_val_type(as_val)) { \
+         as_hashmap_init((as_hashmap *)as_val); \
+     } else if (AS_LIST == as_val_type(as_val)) { \
+         as_arraylist_init((as_arraylist *)as_val); \
+     } else {\
+         status = AEROSPIKE_ERR; \
+         goto exit;     \
+     }
+
+#define AS_ASSIGN_DATA_STORE_TO_PARENT(record_p, val_p, data_store_type_p) \
+     if (record_p) { \
+         
+static as_status
+aerospike_transform_fill_as_map_list(HashTable *ht_p, as_data_list_map_struct* pre_stackalloc_data_p,
+                                     as_record* record_p, as_val* val_p)
+{
+    as_status        status = AEROSPIKE_OK;
+    HashPosition     hashPosition_p = NULL;
+    u_int64_t        index_id_u64 = 0;
+    //as_arraylist*    list_p = NULL;
+    //as_hashmap*      hashmap_p = NULL;
+    as_val*          data_store_type_p = NULL;
+    zval**           data_val_pp = NULL;
+
+    if ((!ht_p) || (!pre_stackalloc_data_p)) {
         status = AEROSPIKE_ERR;
         goto exit;
     }
 
-    switch(Z_TYPE_PP());
+    /* check for map OR list as objects to be used */
+    zend_hash_internal_pointer_reset_ex(ht_p, &pointer);
+    if (HASH_KEY_IS_STRING == zend_hash_get_current_key_ex(ht_p, &key_id_p, &key_id_len, &index_id_u64, 0, &hashPosition_p)) {
+        /* map identified */
+        GET_PRE_ALLOC_MAP(data_store_type_p, pre_stackalloc_data_p)
+    } else {
+        /* list identified */
+        GET_PRE_ALLOC_LIST(data_store_type_p, pre_stackalloc_data_p)
+    }
+
+    if (!data_store_type_p) {
+        status = AEROSPIKE_ERR;
+        goto exit;
+    }
+
+    AS_INIT_DATA_STORE(data_store_type_p)
+    AS_ASSIGN_DATA_STORE_TO_PARENT(record_p, val_p, data_store_type_p)
+
+    foreach_hashtable(ht_p, hashPosition_p, data_val_pp) {
+        switch(Z_TYPE_PP(data_val_pp)) {
+            case IS_LONG:
+                AS_APPEND_INT(data_store_type_p, Z_LVAL_PP(data_val_pp))
+                break;
+            case IS_STRING:
+                AS_APPEND_STR(data_store_type_p, Z_STRVAL_PP(data_val_pp))
+                break;
+            case IS_ARRAY:
+#if 0
+                inner_arr_hash = Z_ARRVAL_PP(data);
+                zend_hash_internal_pointer_reset_ex(inner_arr_hash, &inner_pointer);
+
+                if (zend_hash_get_current_key_ex(inner_arr_hash, &inner_key, &inner_arrkey_len, &inner_index, 0, &inner_pointer) == HASH_KEY_IS_STRING) {
+                    inner_map = as_hashmap_new(32);
+                    as_map *inner_m = (as_map *) inner_map;
+                    handle_put_map(data, inner_m);
+                    as_arraylist_append_map(list, inner_m);
+                } else {
+                    inner_array_len = zend_hash_num_elements(inner_arr_hash);
+                    inner_list = as_arraylist_new(inner_array_len, 0);
+                    handle_put_list(data, inner_list);
+                    as_arraylist_append_list(list, (as_list *) inner_list);
+                }
+#endif
+                break;
+            default:
+                status = AEROSPIKE_ERR;
+                break;
+        }
+    }
+
+exit:
+    return status;
 }
 
 static as_status
-aerospike_transform_key_data(HashTable* ht_p)
+aerospike_transform_fill_asrecord(HashTable* ht_p, as_record* record_p, int8_t* bin_name_p, zval** bin_value_pp,
+                                  as_data_list_map_struct*  pre_stackalloc_data_p)
 {
-    as_status        status = AEROSPIKE_OK;
-    HashPosition     hashPosition_p = NULL;
-    zval**           record_pp = NULL;
+    as_status         status = AEROSPIKE_OK;
+
+    if ((!ht_p) || (!bin_value_pp) || (!bin_name_p) || (!record_p) || (!pre_stackalloc_data)) {
+        status = AEROSPIKE_ERR;
+        goto exit;
+    }
+
+    switch(Z_TYPE_PP(bin_value_pp)) {
+        case IS_NULL:
+            as_record_set_nil(record_p, bin_name_p);
+            break;
+        case IS_LONG:
+            as_record_set_int64(record_p, bin_name_p, (int64_t) Z_LVAL_PP(bin_value_pp));
+            break;
+        case IS_STRING:
+            as_record_set_str(record_p, bin_name_p, (int8_t *) Z_STRVAL_PP(bin_value_pp));
+            break;
+        case IS_ARRAY:
+            break;
+        default:
+            status = AEROSPIKE_ERR;
+            break;
+    }
+}
+
+static as_status
+aerospike_transform_iterate_records(HashTable* ht_p, as_record* record_p, as_data_list_map_struct*  pre_stackalloc_data_p)
+{
+    as_status          status = AEROSPIKE_OK;
+    HashPosition       hashPosition_p = NULL;
+    zval**             record_pp = NULL;
+
+    if ((!ht_p) || !(record_p) || !(pre_stackalloc_data)) {
+        status = AEROSPIKE_ERR;
+        goto exit;
+    }
 
     foreach_hashtable(ht_p, hashPosition_p, record_pp) {
         u_int32_t     bin_name_len_u32 = 0;
         int8_t*       bin_name_p = NULL;
-        u_int32_t     key_type_u32 = zend_hash_get_current_key_ex(ht_p,
-                                                                  &bin_name_p, &bin_name_len,
-                                                                  &index, 0, &pointer);
+        u_int64_t     index_u64 = 0;
+        u_int32_t     key_type_u32 = zend_hash_get_current_key_ex(ht_p, &bin_name_p, &bin_name_len_u32,
+                                                                  &index_u64, 0, &hashPosition_p);
 
-        
+        if (AEROSPIKE_OK != (status = aerospike_transform_fill_asrecord(ht_p, record_p, bin_name_p, 
+                                                                        record_pp, pre_stackalloc_data_p))) {
+            goto exit;
+        }
+
     }
 
 exit:
@@ -352,14 +487,15 @@ exit:
 extern as_status
 aerospike_transform_key_data_put(HashTable* ht_p, as_record* record_p, as_key* as_key_p)
 {
-    as_status        status = AEROSPIKE_OK;
+    as_status                   status = AEROSPIKE_OK;
+    as_data_list_map_struct     list_map_data = {0};
 
     if ((!ht_p) || (!record_p) || (!as_key_p)) {
         status = AEROSPIKE_ERR;
         goto exit;
     }
 
+
 exit:
     return status;
 }
-#endif
