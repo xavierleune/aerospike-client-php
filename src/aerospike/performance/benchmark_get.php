54baefe98237a7f0eee981b1c836c6311089d394
PavanRao-GSLab <pavan.rao@gslab.com>
2014-08-07 16:03:27 +0100

    Update Performance Scripts for latest code

--- src/aerospike/performance/benchmark_get.php
+++ src/aerospike/performance/benchmark_get.php
@@ -1,25 +1,54 @@
 <?php
+$host_ip = "172.20.25.179"
+$host_port = 3000;
 $keys = $argv[1];
-if ($keys < 10000) {
-	echo "Enter first argument greater then or equal to 10000.\n";
+$benchmark_time_after_keys = 100000;
+
+if ($keys < 500000) {
+    echo "Enter first argument greater then or equal to 500000.\n";
 } else {
- $config = array("hosts"=>array(array("name"=>"10.71.71.49", "port"=>3000)));
- $line = "";	
- $as = new Aerospike($config);
- $r = array();
- $avg=0;
- $count = 0;
-	for($i=10000; $i<=$keys; $i++)
-	{
-	   $key = array("ns"=>"test", "set"=>"demo", "key"=>"$i");
-	   $start = time();
-	   $as->get($key, &$r);
-	   $end = time();
-	   $avg = $avg+($end-$start);
-	   $count++;
-	}
- $average_get_time = $avg/$count;
- echo "\nTotal GET Time:= ".$avg."\n";
- echo "Total AVG GET Time:= ".$average_get_time."\n"; 
+    $handle = fopen ("benchmark_get_report.log", 'a+');
+    $config = array("hosts"=>array(array("addr"=>$host_ip, "port"=>$host_port)));
+    $db = new Aerospike($config);
+
+    if (!$db->isConnected()) {
+        echo "Aerospike failed to connect[{$db->errorno()}]: {$db->error()}\n";
+        exit(1);
+    }
+
+    $total_time = 0;
+
+    for($i=0; $i<=$keys; $i++) {
+        $key = $db->initKey("test", "demo", "$i");
+        if (!$key) {
+            echo "Aerospike initKeyfailed[{$db->errorno()}]: {$db->error()}\n";
+            exit(1);
+        }
+
+        //$mem_usage_start = memory_get_usage();
+        $start = microtime(true);
+        $status = $db->get($key, $record);
+        if ($status != Aerospike::OK) {
+            echo "Aerospike get failed[{$db->errorno()}]: {$db->error()}\n";
+            exit(1);
+        }
+
+        $end = microtime(true);
+        $total_time += ($end - $start);
+        //$mem_usage_end = memory_get_usage();
+	if ($i % $benchmark_time_after_keys == 0) {
+            fwrite($handle, "\nGET TIME = " .$total_time. " seconds FOR ". $i. " KEYS\n");
+        }
+    }
+
+    $average_get_time = $total_time/$keys;
+    $read_tps = $keys/$total_time;
+
+    fwrite($handle, "\nTOTAL GET TIME = " .$total_time. " seconds FOR ". $keys. " KEYS\n");
+    fwrite($handle, "AVERAGE GET TIME PER KEY = ". $average_get_time. " seconds\n");
+    fwrite($handle, "AVERAGE READS PER SECOND = ". $read_tps. "\n");
+    fclose($handle);
+    echo "Check the performance report: benchmark_get_report.log";
 }
 ?>
+
--- src/aerospike/performance/benchmark_put.php
+++ src/aerospike/performance/benchmark_put.php
@@ -1,32 +1,59 @@
 <?php
+$host_ip = "172.20.25.179"
+$host_port = 3000;
 $keys = $argv[1];
 $numberofbins = $argv[2];
-$bins_key_value_array=array();
+$bins_key_value_array = array();
+$benchmark_time_after_keys = 100000;
 
-if ($keys < 10000) {
-	echo "Enter first argument greater then or equal to 10000.\n";
+if ($keys < 500000) {
+    echo "Enter first argument greater then or equal to 500000.\n";
 } else {
-	$config = array("hosts"=>array(array("name"=>"10.71.71.49", "port"=>3000)));
-	$as = new Aerospike($config);
-	$avg=0;
-        $count = 0;
-	$line="";
-	for($i=10000;$i<=$keys;$i++)
-	{
-		$key = array("ns"=>"test", "set"=>"demo", "key"=>"$i");
-		for($j=1; $j<=$numberofbins; $j++)
-		{
-			$value = $i+$j;
-			$bins_key_value_array["bin".$j] = "$value";
-		}
-               $start = time();
-               $rv = $as->put($key, $bins_key_value_array);
-	       $end = time();
-               $avg = $avg+($end-$start);
-               $count++;
+    $handle = fopen ("benchmark_put_report.log", 'a+');
+    $config = array("hosts"=>array(array("addr"=>$host_ip, "port"=>$port)));
+    $db = new Aerospike($config);
+
+    if (!$db->isConnected()) {
+        echo "Aerospike failed to connect[{$db->errorno()}]: {$db->error()}\n";
+        exit(1);
+    }
+
+    $total_time = 0;
+    $line = "";
+    $time_at_every_ = array();
+
+    for($i=0; $i<=$keys; $i++) {
+        $key = $db->initKey("test", "demo", "$i");
+        if (!$key) {
+            echo "Aerospike initKeyfailed[{$db->errorno()}]: {$db->error()}\n";
+            exit(1);
         }
-        $average_put_time = $avg/$count;
-        echo "\nTotal PUT Time:= ".$avg."\n";
-        echo "Total AVG PUT Time:= ".$average_put_time."\n";
+
+        for($j=1; $j<=$numberofbins; $j++) {
+            $value = $i + $j;
+            $bins_key_value_array["bin".$j] = "$value";
+        }
+
+        $start = microtime(true);
+        $status = $db->put($key, $bins_key_value_array);
+        if ($status != Aerospike::OK) {
+            echo "Aerospike put failed[{$db->errorno()}]: {$db->error()}\n";
+            exit(1);
+        }
+
+        $end = microtime(true);
+        $total_time += ($end - $start);
+	if ($i % $benchmark_time_after_keys == 0) {
+	    fwrite($handle, "\nPUT TIME = " .$total_time. " seconds FOR ". $i. " KEYS\n");
+	}
+    }
+
+    $average_put_time = $total_time/$keys;
+    $write_tps = $keys/$total_time;
+    fwrite($handle, "\nTOTAL PUT TIME = " .$total_time. " seconds FOR ". $keys. " KEYS\n");
+    fwrite($handle, "AVERAGE PUT TIME PER KEY = ". $average_put_time. " seconds\n");
+    fwrite($handle, "AVERAGE WRITES PER SECOND = ". $write_tps. "\n");
+    fclose($handle);
+    echo "Check the performance report: benchmark_put_report.log";
 }
 ?>
