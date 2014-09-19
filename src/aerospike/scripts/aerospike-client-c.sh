19a54f5e388c4a9519d61850834ee23a7405d7fd
Ronen Botzer <rbotzer@yahoo.com>
2014-09-19 16:47:43 -0700

    Attempt to sudo create the lua paths

--- src/aerospike/scripts/aerospike-client-c.sh
+++ src/aerospike/scripts/aerospike-client-c.sh
@@ -402,10 +402,14 @@ if [ ! -d $LUA_SYSPATH ]; then
 fi
 
 if [ ! -d $LUA_SYSPATH ]; then
-    printf "Failed to create the Lua system files path.  Please run:\n"
-    printf "    sudo scripts/lua-paths.sh ${LUA_SYSPATH_PREFIX} ${AEROSPIKE_C_CLIENT}\n"
-    printf "Then re-run this scripts:\n    ./build.sh\n"
-    exit 1
+    sudo scripts/lua-paths.sh ${LUA_SYSPATH_PREFIX} ${AEROSPIKE_C_CLIENT}
+    if [ $? -gt 0]; then
+        printf "Failed to create the Lua system files path.  Please run:\n"
+        printf "    sudo scripts/lua-paths.sh ${LUA_SYSPATH_PREFIX} ${AEROSPIKE_C_CLIENT}\n"
+        printf "Then re-run this scripts:\n    ./build.sh\n"
+        exit 1
+    fi
+    sudo cp ${AEROSPIKE_LUA}/*.lua ${LUA_SYSPATH}
 fi
 
 if [ ! -f ${LUA_SYSPATH}/aerospike.lua ]; then
