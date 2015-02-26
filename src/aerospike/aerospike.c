8e80d8a3600d6075395f39bec03311d56bc6627b
PavanRao-GSLab <pavan.rao@gslab.com>
2015-02-26 11:38:52 +0530

    Removed a compilation warning

--- src/aerospike/aerospike.c
+++ src/aerospike/aerospike.c
@@ -110,7 +110,7 @@ static void aerospike_check_close_and_destroy(void *hashtable_element) {
             if (as_ref_p->as_p) {
                 int iter_hosts = 0;
                 for (iter_hosts = 0; iter_hosts < as_ref_p->as_p->config.hosts_size; iter_hosts++) {
-                    pefree(as_ref_p->as_p->config.hosts[iter_hosts].addr, 1);
+                    pefree((char *) as_ref_p->as_p->config.hosts[iter_hosts].addr, 1);
                 }
                 if (AEROSPIKE_OK != aerospike_close(as_ref_p->as_p, &error)) {
                     DEBUG_PHP_EXT_ERROR("Aerospike close returned error");
@@ -400,7 +400,7 @@ static void Aerospike_object_free_storage(void *object TSRMLS_DC)
             }
             int iter_hosts = 0;
             for (iter_hosts = 0; iter_hosts < intern_obj_p->as_ref_p->as_p->config.hosts_size; iter_hosts++) {
-                pefree(intern_obj_p->as_ref_p->as_p->config.hosts[iter_hosts].addr, 1);
+                pefree((char *) intern_obj_p->as_ref_p->as_p->config.hosts[iter_hosts].addr, 1);
             }
             aerospike_destroy(intern_obj_p->as_ref_p->as_p);
             intern_obj_p->as_ref_p->as_p = NULL;
