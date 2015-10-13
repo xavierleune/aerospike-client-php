3b2527a6c7689a6c337f8d842d95c685e984ccd3
PratimaMane-GSLab <pratima.mane@gslab.com>
2015-10-13 15:04:01 +0530

    Fixed CLIENT-69 issue of a put() with no data returns Aerospike::OK also added test cases for this issue

--- src/aerospike/aerospike_transform.c
+++ src/aerospike/aerospike_transform.c
@@ -2847,6 +2847,7 @@ aerospike_transform_key_data_put(aerospike* as_object_p,
     as_record                   record;
     int16_t                     init_record = 0;
     uint16_t                    gen_value = 0;
+    int                         num_of_bins = 0;
 
     if ((!record_pp) || (!as_key_p) || (!error_p) || (!as_object_p)) {
         DEBUG_PHP_EXT_DEBUG("Unable to put record");
@@ -2854,7 +2855,15 @@ aerospike_transform_key_data_put(aerospike* as_object_p,
         goto exit;
     }
 
-    as_record_inita(&record, zend_hash_num_elements(Z_ARRVAL_PP(record_pp)));
+    num_of_bins = zend_hash_num_elements(Z_ARRVAL_PP(record_pp));
+    if (num_of_bins < 1) {
+        error_p->code = AEROSPIKE_ERR_PARAM;
+        DEBUG_PHP_EXT_DEBUG("Record must be given as bin => val pair");
+        PHP_EXT_SET_AS_ERR(error_p, AEROSPIKE_ERR_PARAM, "Record must be given as bin => val pair");
+        goto exit;
+    }
+
+    as_record_inita(&record, num_of_bins);
     init_record = 1;
 
     set_policy(&as_object_p->config, NULL, &write_policy, NULL, NULL, NULL, NULL, NULL,
