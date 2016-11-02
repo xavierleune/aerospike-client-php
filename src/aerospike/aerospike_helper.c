1838bbfab9bfc2ce51bf968ed82a3d54ba78ef55
Nicolas Payart <npayart@ccmbenchmark.com>
2016-11-02 15:02:24 +0100

    Do not check save_handler value. It's not necessary and may be altered by PHP

--- src/aerospike/aerospike_helper.c
+++ src/aerospike/aerospike_helper.c
@@ -963,21 +963,19 @@ aerospike_helper_check_and_set_config_for_session(as_config *config_p,
 
 	char *save_handler = SAVE_HANDLER_PHP_INI;
 	if (save_handler != NULL) {
-		if (!strncmp(save_handler, AEROSPIKE_SESSION, AEROSPIKE_SESSION_LEN)) {
-			if (!save_path) {
-				save_path = SAVE_PATH_PHP_INI;
-			}
+		if (!save_path) {
+			save_path = SAVE_PATH_PHP_INI;
+		}
 
-			if (save_path) {
-				if (AEROSPIKE_OK != parse_save_path(save_path, session_p,
-					config_p, error_p TSRMLS_CC)) {
-					goto exit;
-				}
-			} else {
-				PHP_EXT_SET_AS_ERR(error_p, AEROSPIKE_ERR_CLIENT, "Could not read SAVE_PATH settings");
-				DEBUG_PHP_EXT_ERROR("Could not read SAVE_PATH settings");
+		if (save_path) {
+			if (AEROSPIKE_OK != parse_save_path(save_path, session_p,
+				config_p, error_p TSRMLS_CC)) {
 				goto exit;
 			}
+		} else {
+			PHP_EXT_SET_AS_ERR(error_p, AEROSPIKE_ERR_CLIENT, "Could not read SAVE_PATH settings");
+			DEBUG_PHP_EXT_ERROR("Could not read SAVE_PATH settings");
+			goto exit;
 		}
 	} else {
 		PHP_EXT_SET_AS_ERR(error_p, AEROSPIKE_ERR_CLIENT, "Could not read SAVE_HANDLER settings");
